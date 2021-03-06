6712c683385b36ed2d3aa832353677c58c31a513
anuj <anujbidkar18@icloud.com>
2021-03-06 06:30:36 +0530

    new register

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,42 +1,97 @@
 import Navbar_component from '../Components/Navbar_component';
-import {BrowserRouter,Switch,Route,Link} from "react-router-dom";
+import React, { useState } from "react";
+import {signup} from './API';
+import { useHistory } from "react-router";
+
+import {BrowserRouter,Switch,Route,Link,Redirect} from "react-router-dom";
 
 function Register()
 {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error:false,
+        success: false
+      });
+
+    const [datafromBackend, datafromBackendadd] = useState();
+    
+      const { name, email, password, error, success } = values;
+    
+      const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+      };
+    
+      const onSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: false });
+        signup({ name, email, password })
+          .then(data => {
+            if (data.error) 
+            {
+              setValues({ ...values, error: data.error, success: false });
+            } else 
+            {
+            
+
+             datafromBackendadd(data);
+                console.log(data);
+              setValues({
+                ...values,
+                name: "",
+                email: "",
+                password: "",
+                error: false,
+                success: true
+              });
+
+              this.props.history.push('/');
+            //   <Redirect  to='/login' />
+            }
+          })
+        //   .catch(console.log("Error in signup"));
+      };
+    
     return(<div>
         <Navbar_component />
 
         <div className="row">
+
 
                     <div className="col-lg-4 "></div>
                     <div className="col-lg-4 my_login_div ">
                         <h1 className="text-center">Register    </h1>
 
                         <div className="form-group">
+                            <label >Name <span className="text-danger">*</span></label>
+                            <input type="text" value={name} onChange={handleChange("name")} required placeholder="Enter Your Name" className="form-control"/>
+                        </div>
+                        <div className="form-group">
                             <label >Email <span className="text-danger">*</span></label>
-                            <input type="email" required placeholder="Enter Your Email" className="form-control"/>
+                            <input type="email" value={email} onChange={handleChange("email")} required placeholder="Enter Your Email" className="form-control"/>
                         </div>
                         <div className="form-group">
                             <label >Password <span className="text-danger">*</span></label>
-                            <input type="password" required placeholder="Enter Your Password" className="form-control"/>
+                            <input type="password"  value={password} onChange={handleChange("password")} required placeholder="Enter Your Password" className="form-control"/>
                         </div>
+                       
                         <div className="form-group">
-                            <label >Mobile <span className="text-danger">*</span></label>
-                            <input type="number" required placeholder="Enter Your Mobile" className="form-control"/>
+                        {JSON.stringify(values)}
                         </div>
+
                         <div className="form-group">
-                            <label >Address <span className="text-danger">*</span></label>
-                            <input type="text" required placeholder="Enter Your Address" className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <button className="btn btn-danger mx-auto d-block">
+                            <button  onClick={onSubmit} className="btn btn-danger mx-auto d-block">
                                 Register
                             </button>
+                            {JSON.stringify(datafromBackend)}
                         </div>
                        
 
                     </div>
-                    <div className="col-lg-4 "></div>
+                    <div className="col-lg-4 ">    
+                    <p className="text-light text-center">{JSON.stringify(values)}</p>    <p className="text-light text-center">{JSON.stringify(values)}</p></div>
+                
 
 
 
